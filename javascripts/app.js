21c0c1bd18231d83c20551f8588bacf0ab4a37f1
Nicholas Guzman <nguzman23193@columbushs.com>
2020-12-14 15:12:16 -0500

    end

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,15 +1,51 @@
 // Rover Object Goes Here
 // ======================
-
+let rover = {
+  direction: "N"
+}
+let direct = rover.direction;
 // ======================
 function turnLeft(rover){
   console.log("turnLeft was called!");
+  if(direct == `N`){
+     let direct = `W`;
+     console.log(`Rover is facing Noth and turns left => Rover is now facing West`);
+  } else if(direct == `W`) {
+     let direct = `S`
+     console.log(`Rover is facing West and turns left => Rover is now facing South`)
+  } else if (direct == `S`) {
+     let direct = `E`
+     console.log(`Rover is facing South and turns left => Rover is now facing East`)
+  } else if(direct == `E`) {
+     let direct = `N`
+     console.log(`Rover is facing East and turns left => Rover is now facing North`)
+  }
 }
+
+
+
 
 function turnRight(rover){
   console.log("turnRight was called!");
+  if(direct == `N`){
+    let direct = `E`;
+    console.log(`Rover is facing Noth and turns right => Rover is now facing East`);
+ } else if(direct == `E`) {
+    let direct = `S`
+    console.log(`Rover is facing East and turns left => Rover is now facing South`)
+ } else if (direct == `S`) {
+    let direct = `W`
+    console.log(`Rover is facing South and turns left => Rover is now facing West`)
+ } else if(direct == `W`) {
+    let direct = `N`
+    console.log(`Rover is facing West and turns left => Rover is now facing North`)
+ }
 }
+
+
 
 function moveForward(rover){
   console.log("moveForward was called")
+  
 }
+ 
